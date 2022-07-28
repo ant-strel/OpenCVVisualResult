6f79ae6dab059221b9d6940678132170c4393ba5
Anton <d-antes@mail.ru>
2022-07-28 16:33:46 +0300

    Добавьте файлы проекта.

---
+++ OpenCVVisualResult/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OpenCVVisualResult
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    //список функций
+    //кнопка обработать
+
+    //исходное изображение, фиксированное.
+    //параметры исходника
+
+    //источник - исходное изображение либо результат
+    //параметры источника
+
+    //результат обработки
+    //параметры результата
+
+    //параметры сохранения(имя, формат)
+    //кнопка сохранить
+
+    //лог
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
